e32fb4edf8e50abf3ec8966a5f18acddfde62ac6
Reactflyweis <react3@flyweis.technology>
2023-09-20 10:59:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [result, setResult] = useState();
+
+  const handlenum1change = (e) => {
+    setNum1(e.target.value);
+  };
+
+  const handlenum2change = (e) => {
+    setNum2(e.target.value);
+  };
+
+  const Displayresult = () => {
+    const resultvalue = parseFloat(num1) + parseFloat(num2);
+    setResult(resultvalue);
+    setNum1("");
+    setNum2("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="number"
+        placeholder="Enter first number"
+        value={num1}
+        onChange={handlenum1change}
+      />
+
+      <input
+        type="number"
+        placeholder="Enter second number"
+        value={num2}
+        onChange={handlenum2change}
+      />
+      <button className="btn" onClick={Displayresult}>
+        Display Result
+      </button>
+      <input type="text" placeholder="Result" value={result} readOnly />
     </div>
   );
 }
